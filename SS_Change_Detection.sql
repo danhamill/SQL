5f1b507b74a44a33e3bb8683ccd0ce96f9e61ea2
danhamill <hamill.daniel@gmail.com>
2015-11-22 18:03:15 -0700

    first commit

---
+++ SS_Change_Detection.sql
@@ -0,0 +1,3 @@
+﻿SELECT r4a_2015_04.easting, r4a_2015_04.northing, r4a_2015_04.texture-r4a_2014_09.texture as texture_diff
+FROM r4a_2015_04,r4a_2014_09
+WHERE r4a_2015_04.easting = r4a_2014_09.easting AND r4a_2015_04.northing = r4a_2014_09.northing;
