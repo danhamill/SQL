d70deabd091002700a1ee4d4515071368df183fa
danhamill <hamill.daniel@gmail.com>
2015-11-22 17:18:11 -0700

    fisrt commit

---
+++ Select_N_E_Texture.sql
@@ -0,0 +1,4 @@
+﻿SELECT r4a_2015_04.easting, r4a_2015_04.northing, r4a_2015_04.texture
+FROM r4a_2015_04
+INNER JOIN r4a_2014_09
+ON  r4a_2015_04.easting = r4a_2014_09.easting AND r4a_2015_04.northing = r4a_2014_09.northing;
