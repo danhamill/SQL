7e36847dd959cbbfa7531571d7ab4863f2622d61
danhamill <hamill.daniel@gmail.com>
2016-06-11 16:32:33 -0700

    first commit

---
+++ Group_mb_ss.sql
@@ -0,0 +1,10 @@
+﻿SELECT * 
+from mb_may_2012_3m tt
+inner join
+(
+	SELECT s.easting, s.northing, s.texture, s.sidescan_intensity 
+	FROM ss_2012_05 s
+	
+) ss
+	on tt.easting=ss.easting 
+	and tt.northing=ss.northing;
